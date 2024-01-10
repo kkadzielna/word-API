91c28c906d4c079fcbc275b5756de0002e60b0c4
kkadzielna <k.kadzielawa00@gmail.com>
2024-01-10 17:16:57 +0000

    read in file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # word-API
+
+A Python Flask API that reads in a plain text file, counts the number of words, the average word length, the number of words at each length, and the most frequent word length.
+
+A word is any set of letters separated by white spaces. Numbers and other sets of non-letter characters do not count as words.
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -6,8 +7,20 @@ app = Flask(__name__)
 def home():
     return "Home"
 
+@app.route("/read", methods=["GET"])
+def read():
+    file_name = request.args.get("file_name", "data/nofile.txt")
+    with open(file_name, 'r') as f:
+        file_content = f.read()
+    return file_content
+
+def count_words()
+
 if __name__ == "__main__":
     app.run(debug=True)
 
 #sudo docker build -t flaskapi:latest .
 #sudo docker run -p 5000:5000 flaskapi:latest
+
+#sudo docker-compose build
+#sudo docker-compose up
